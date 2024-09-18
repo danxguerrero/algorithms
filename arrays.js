a56c00193df26065b844bf3009ea29d432617e8b
Daniel Guerrero <danxguerrero@yahoo.com>
2024-09-18 19:05:54 -0400

    Compress solution

--- arrays.js
+++ arrays.js
@@ -126,3 +126,36 @@ const uncompress = (s) => {
       }
     }
 }
+
+// compress
+// Write a function, compress, that takes in a string as an argument. The function should return a compressed version of the string where consecutive occurrences of the same characters are compressed into the number of occurrences followed by the character. Single character occurrences should not be changed.
+
+// 'aaa' compresses to '3a'
+// 'cc' compresses to '2c'
+// 't' should remain as 't'
+
+// You can assume that the input only contains alphabetic characters.
+
+const compress = (s) => {
+    let result = []
+    let i = 0
+    let j = 1
+    let count = 1
+  
+    while ( j < s.length + 1) {
+      if (s[j] == s[i]) {
+        count++
+        j++
+      } else {
+        if (count > 1) {
+          result.push(count)
+        }
+        result.push(s[i])
+        i=j
+        j++
+        count = 1   
+      }
+  }
+  
+    return result.join('')
+};
