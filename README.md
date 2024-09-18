71b036b2c1ab34da389173e39c20b1ab5632309d
Daniel Guerrero <danxguerrero@yahoo.com>
2024-09-18 18:59:32 -0400

    Anagrams solution

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # algorithms
 This is my repo for learning and practicing algorithms
 
-I will be practicing various data structures and algorithms in Several different languages. 
+I will be practicing various data structures and algorithms in Several different languages.
---
+++ arrays.js
@@ -0,0 +1,33 @@
+// anagrams
+// Write a function, anagrams, that takes in two strings as arguments. The function should return a boolean indicating whether or not the strings are anagrams. Anagrams are strings that contain the same characters, but in any order.
+
+const anagrams = (s1, s2) => {
+
+    if (s1.length != s2.length) {
+      return false
+    }
+    
+    const seenLetters = {}
+  
+    for (i=0; i < s1.length; i++) {
+      if(!seenLetters[s1[i]]) {
+        seenLetters[s1[i]] = 1
+      } else {
+        seenLetters[s1[i]]++
+      }
+    }
+  
+    for (i=0; i < s2.length; i++) {
+      if (seenLetters[s2[i]] > 0) {
+        seenLetters[s2[i]]--
+      } else {
+        return false
+      }
+    }
+  
+    return true
+  };
+  
+  module.exports = {
+    anagrams,
+  };
