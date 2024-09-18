7d41ba9c5f392c10a70ada83feb016a72862f8c0
Daniel Guerrero <danxguerrero@yahoo.com>
2024-09-18 19:00:14 -0400

    Most Frequent Char solution

--- arrays.js
+++ arrays.js
@@ -31,3 +31,8 @@ const anagrams = (s1, s2) => {
   module.exports = {
     anagrams,
   };
+
+// most frequent char
+// Write a function, mostFrequentChar, that takes in a string as an argument. The function should return the most frequent character of the string. If there are ties, return the character that appears earlier in the string.
+
+// You can assume that the input string is non-empty.
