99d4f0a29fbbc9038ba6d8768fde98cf417a02c1
Daniel Guerrero <danxguerrero@yahoo.com>
2024-09-18 19:06:49 -0400

    Intersection Solution

--- arrays.js
+++ arrays.js
@@ -159,3 +159,12 @@ const compress = (s) => {
   
     return result.join('')
 };
+
+// intersection
+// Write a function, intersection, that takes in two arrays, a,b, as arguments. The function should return a new array containing elements that are in both of the two arrays.
+
+// You may assume that each input array does not contain duplicate elements.
+
+const intersection = (a, b) => {
+    return a.filter(x => b.includes(x))
+}
