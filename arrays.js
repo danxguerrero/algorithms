994d50e7807cb2faff142d24da8c34f3402aae19
Daniel Guerrero <danxguerrero@yahoo.com>
2024-09-18 19:01:13 -0400

    Pair Sum solution

--- arrays.js
+++ arrays.js
@@ -36,3 +36,53 @@ const anagrams = (s1, s2) => {
 // Write a function, mostFrequentChar, that takes in a string as an argument. The function should return the most frequent character of the string. If there are ties, return the character that appears earlier in the string.
 
 // You can assume that the input string is non-empty.
+
+const mostFrequentChar = (s) => {
+    const letters = {}
+  
+    for (i=0; i < s.length; i++) {
+      if (!letters[s[i]]) {
+        letters[s[i]] = 1
+      } else {
+        letters[s[i]]++
+      }
+    }
+  
+    const keys = Object.keys(letters)
+    let max = keys[0]
+    for (i=0; i < keys.length; i++) {
+      if (letters[keys[i]] > letters[max]) {
+        max = keys[i]
+      }
+    }
+  
+    return max
+  };
+  
+  console.log(mostFrequentChar('bookeeper'))
+  
+  module.exports = {
+    mostFrequentChar,
+  };
+
+// pair sum
+// Write a function, pairSum, that takes in an array and a target sum as arguments. The function should return an array containing a pair of indices whose elements sum to the given target. The indices returned must be unique.
+
+// Be sure to return the indices, not the elements themselves.
+
+// There is guaranteed to be one such pair that sums to the target.
+
+const pairSum = (numbers, targetSum) => {
+    seenNumbers = {}
+  
+    for (i=0;i<numbers.length;i++) {
+      const compliment = targetSum - numbers[i]
+  
+      if (seenNumbers[compliment] != undefined) {
+        return [seenNumbers[compliment], i]
+      }
+      
+      seenNumbers[numbers[i]] = i
+    }
+    return
+  };
