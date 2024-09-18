1a5342d7d70cf8d82d3f960774c1542cd3366108
Daniel Guerrero <danxguerrero@yahoo.com>
2024-09-18 19:04:38 -0400

    Uncompress solution

--- arrays.js
+++ arrays.js
@@ -97,4 +97,32 @@ const pairProduct = (numbers, targetProduct) => {
       }
     }
     return
-  };
+};
+
+// uncompress
+// Write a function, uncompress, that takes in a string as an argument. The input string will be formatted into multiple groups according to the following pattern:
+
+// <number><char>
+
+// for example, '2c' or '3a'.
+
+// The function should return an uncompressed version of the string where each 'char' of a group is repeated 'number' times consecutively. You may assume that the input string is well-formed according to the previously mentioned pattern.
+
+const uncompress = (s) => {
+    let result = ''
+    let i = 0
+    let j = 0
+  
+    while(j < s.length) {
+      if (s[j].charCodeAt(0) >= 48 && s[j].charCodeAt(0) <= 57) {
+        j++
+      } else {
+        const num = Number(s.slice(i,j))
+        for (let count = 0; count < num; count++) {
+          result += s[j]
+        }
+        j++
+        i=j
+      }
+    }
+}
