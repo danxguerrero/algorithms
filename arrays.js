0e003190142deaebe3be85b0ac4816e2874c8804
Daniel Guerrero <danxguerrero@yahoo.com>
2024-09-20 11:50:13 -0400

    349. Intersection of Two Arrays Solution

--- arrays.js
+++ arrays.js
@@ -214,3 +214,46 @@ function fiveSort(nums) {
   // Q: Given an array of n elements, how many different triplets of elements can be formed in 
   // terms of Big-O?
   // A: There are O(n^3) possible triplets
+  
+
+
+  /*
+    LeetCode problems start here:
+  */
+
+  // 349. Intersection of Two Arrays
+  // Given two integer arrays nums1 and nums2, return an array of their 
+  // intersection.
+  // Each element in the result must be unique and you may return the result in any order.
+
+  
+
+  // Example 1:
+
+  // Input: nums1 = [1,2,2,1], nums2 = [2,2]
+  // Output: [2]
+  // Example 2:
+
+  // Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+  // Output: [9,4]
+  // Explanation: [4,9] is also accepted.
+  
+
+  // Constraints:
+
+  // 1 <= nums1.length, nums2.length <= 1000
+  // 0 <= nums1[i], nums2[i] <= 1000
+
+  var intersection2 = function(nums1, nums2) {
+    const set1 = new Set(nums1)
+    const set2 = new Set(nums2)
+    const intersect = new Set()
+
+    for (num of set1) {
+        if (set2.has(num)) {
+            intersect.add(num)
+        }
+    }
+    
+    return Array.from(intersect)
+};
