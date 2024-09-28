c25768e85dcb3bd23d00aca7f50471c7a72cec44
Daniel Guerrero <danxguerrero@yahoo.com>
2024-09-27 23:52:04 -0400

    Zipper Lists solution

--- linked-list.js
+++ linked-list.js
@@ -141,3 +141,59 @@ const reverseList2 = (head) => {
   
   return prev
 };
+
+// zipper lists
+// Write a function, zipperLists, that takes in the head of two 
+// linked lists as arguments. The function should zipper the two 
+// lists together into single linked list by alternating nodes. 
+// If one of the linked lists is longer than the other, 
+// the resulting list should terminate with the remaining nodes. 
+// The function should return the head of the zippered linked list.
+
+// Do this in-place, by mutating the original Nodes.
+
+// You may assume that both input lists are non-empty.
+
+// recursive solution
+// Time: O(min(n,m)), linear
+// Space: O(min(n,m)), linear
+const zipperLists = (head1, head2) => {
+  if (head1 === null && head2 === null) return null
+  if (head1 === null) return head2
+  if (head2 === null) return head1
+
+  const next1 = head1.next
+  const next2 = head2.next
+  head1.next = head2
+  head2.next = zipperLists(next1, next2)
+
+  return head1
+};
+
+// iterative solution
+// Time: O(min(n,m)) => linear time
+// Space: O(1), constant time
+
+const zipperLists2 = (head1, head2) => {
+  let tail = head1
+  let current1 = head1.next
+  let current2 = head2
+  let count = 0
+  
+  while (current1 !== null && current2 !== null) {
+    if ( count % 2 === 0) {
+      tail.next = current2
+      current2 = current2.next
+    } else {
+      tail.next = current1
+      current1 = current1.next
+    }
+    
+    tail = tail.next
+    count++
+  }
+
+  if (current1) tail.next = current1
+  if (current2) tail.next = current2
+  return head1
+};
