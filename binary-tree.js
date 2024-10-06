f32cd58e0118b885fad70d208636b2eccef1b0f8
Daniel Guerrero <danxguerrero@yahoo.com>
2024-10-05 23:17:20 -0400

    Max Root to Leaf Path Sum solution

--- binary-tree.js
+++ binary-tree.js
@@ -192,3 +192,22 @@ const treeMinValue3 = (root) => {
 
   return min
 };
+
+// max root to leaf path sum
+// Write a function, maxPathSum, that takes in the root of a 
+// binary tree that contains number values. The function should 
+// return the maximum sum of any root to leaf path within the 
+// tree.
+
+// You may assume that the input tree is non-empty.
+
+// recursive solution
+// Time: O(n), linear time
+// Space: O(n), linear time
+
+const maxPathSum = (root) => {
+  if (root === null) return -Infinity
+  if (root.left === null && root.right === null) return root.val
+  const maxChildPathSum = Math.max(maxPathSum(root.left), maxPathSum(root.right))
+  return root.val + maxChildPathSum
+};
