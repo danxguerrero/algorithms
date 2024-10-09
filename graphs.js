416b1105b4c4dcc3ae15226c99298a652c746104
Daniel Guerrero <danxguerrero@yahoo.com>
2024-10-09 16:51:18 -0400

    Has Path solution

---
+++ graphs.js
@@ -0,0 +1,42 @@
+// has path
+// Write a function, hasPath, that takes in an object 
+// representing the adjacency list of a directed acyclic 
+// graph and two nodes (src, dst). The function should 
+// return a boolean indicating whether or not there 
+// exists a directed path between the source and 
+// destination nodes.
+
+// recursive solution
+// Time: O(n +edges), linear time 
+// Space: O(n), linear time
+
+const hasPath = (graph, src, dst) => {
+    if (src === dst) return true
+  
+    for (let neighbor of graph[src]) {
+      if (hasPath(graph, neighbor, dst) === true) {
+        return true
+      }
+    }
+    
+    return false
+  };
+
+  // iterative solution
+  // Time: O(n + edges), linear time
+  // Space: O(n), linear time
+  const hasPath2 = (graph, src, dst) => {
+    const queue = [src]
+   
+     while (queue.length > 0) {
+       const current = queue.shift()
+       
+       if (current === dst) return true
+       
+       for (let neighbor of graph[current]) {
+         queue.push(neighbor)
+       }
+     }
+   
+     return false
+   };
