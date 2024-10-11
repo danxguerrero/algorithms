f20230343b09f41e97b2c0c84235f1cc2fcaff20
Daniel Guerrero <danxguerrero@yahoo.com>
2024-10-11 17:30:49 -0400

    226. Invert Binary Tree solution

--- binary-tree.js
+++ binary-tree.js
@@ -211,3 +211,39 @@ const maxPathSum = (root) => {
   const maxChildPathSum = Math.max(maxPathSum(root.left), maxPathSum(root.right))
   return root.val + maxChildPathSum
 };
+
+/**
+ * LeetCode Problems Start Here
+ */
+
+// 226. Invert Binary Tree
+// Given the root of a binary tree, invert the tree, and return its root.
+
+// Example 1:
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+
+// Example 2:
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+// Example 3:
+
+// Input: root = []
+// Output: []
+
+var invertTree = function(root) {
+  if (root === null) return root
+  const queue = [root]
+
+  while (queue.length > 0) {
+      const current = queue.shift()
+      if (current.left !== null) queue.push(current.left)
+      if (current.right !== null) queue.push(current.right)
+      let left = current.left
+      let right = current.right
+      current.left = right
+      current.right = left
+  }
+
+  return root
+};
