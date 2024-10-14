bf609761b9041b920f4308f4c2f8afbb3b061843
Daniel Guerrero <danxguerrero@yahoo.com>
2024-10-14 16:09:25 -0400

    169. Majority Element solution

--- arrays.js
+++ arrays.js
@@ -336,3 +336,61 @@ var generate = function(numRows) {
 
   return triangle    
 };
+
+// 169. Majority Element
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+ 
+
+// Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+var majorityElement = function(nums) {
+  if (nums.length === 1) return nums[0]
+  const target = nums.length / 2
+  const map = {}
+
+  for (num of nums) {
+      if (!map[num]) {
+          map[num] = 1
+      } else {
+          map[num]++
+          if (map[num] > target) return num
+          
+      }
+  }
+};
+
+var majorityElement = function(nums) {
+  if (nums.length === 1) return nums[0]
+  const target = nums.length / 2
+  let count = 1
+  nums.sort()
+
+  for (i=0; i < nums.length; i++) {
+     if (nums[i] == nums[i+1]) {
+              count++
+              if (count >= target) return nums[i]
+     } else {
+      count = 1
+     }
+  }
+};
