6ad7ea98631fadf5a1aa5eabc74c00cc69f3dc63
Daniel Guerrero <danxguerrero@yahoo.com>
2024-10-16 17:05:50 -0400

    Updated 57. Insert Interval solution

--- arrays.js
+++ arrays.js
@@ -490,8 +490,10 @@ var insert = function(intervals, newInterval) {
   while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
     newInterval[0] = Math.min(intervals[i][0], newInterval[0])
     newInterval[1] = Math.max(intervals[i][1], newInterval[1])
+    i++
   }
-
+  result.push(newInterval)
+  
   while (i < intervals.length) {
     result.push(intervals[i])
     i++
