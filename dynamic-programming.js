e0a0f163e629fc964f4f1fd0210d6dd6b4aff791
Daniel Guerrero <danxguerrero@yahoo.com>
2024-10-30 15:52:29 -0400

    Sum Possible solution

--- dynamic-programming.js
+++ dynamic-programming.js
@@ -46,4 +46,23 @@ const tribonacci = (n, memo={}) => {
   return memo[n]
 };
 
-console.log(tribonacci(2));
+// sum possible
+// Write a function sumPossible that takes in an amount and an array of positive numbers. The function should return a boolean indicating whether or not it is possible to create the amount by summing numbers of the array. You may reuse numbers of the array as many times as necessary.
+
+// You may assume that the target amount is non-negative.
+
+const sumPossible = (amount, numbers, memo={}) => {
+  if (amount in memo) return memo[amount];
+  if (amount < 0) return false;
+  if (amount === 0) return true;
+
+  for (let num of numbers) {
+    if (sumPossible(amount - num, numbers, memo) === true) {
+      memo[amount] = true;
+      return true;
+    }
+  }
+
+  memo[amount] = false;
+  return false;
+};
