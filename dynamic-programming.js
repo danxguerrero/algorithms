010768879dc1a84019432cbbce251a87abf1e925
Daniel Guerrero <danxguerrero@yahoo.com>
2024-10-30 17:13:55 -0400

    Formatted dynamic-programming.js to fit within 80 columns

--- dynamic-programming.js
+++ dynamic-programming.js
@@ -6,7 +6,7 @@
 
 // The 1-st number of the sequence is 1.
 
-// To generate further numbers of the sequence, calculate the sum of previous 
+// To generate further numbers of the sequence, calculate the sum of previous
 // two numbers.
 
 // Solve this recursively.
@@ -24,37 +24,43 @@ const fib = (n, memo = {}) => {
 };
 
 //tribonacci
-// Write a function tribonacci that takes in a number argument, n, and returns the n-th number of the Tribonacci sequence.
+// Write a function tribonacci that takes in a number argument, n, and returns 
+// the n-th number of the Tribonacci sequence.
 
 // The 0-th and 1-st numbers of the sequence are both 0.
 
 // The 2-nd number of the sequence is 1.
 
-// To generate further numbers of the sequence, calculate the sum of previous three numbers.
+// To generate further numbers of the sequence, calculate the sum of previous
+// three numbers.
 
 // Solve this recursively.
 
 // Time: O(3^n)
 // Space: O(n)
 
-const tribonacci = (n, memo={}) => {
+const tribonacci = (n, memo = {}) => {
   if (n in memo) return memo[n];
   if (n === 0 || n === 1) return 0;
   if (n === 2) return 1;
 
-  memo[n] = tribonacci(n-1, memo) + tribonacci(n-2, memo) + tribonacci(n - 3, memo);
-  return memo[n]
+  memo[n] =
+    tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo);
+  return memo[n];
 };
 
 // sum possible
-// Write a function sumPossible that takes in an amount and an array of positive numbers. The function should return a boolean indicating whether or not it is possible to create the amount by summing numbers of the array. You may reuse numbers of the array as many times as necessary.
+// Write a function sumPossible that takes in an amount and an array of positive 
+// numbers. The function should return a boolean indicating whether or not it is 
+// possible to create the amount by summing numbers of the array. You may reuse 
+// numbers of the array as many times as necessary.
 
 // You may assume that the target amount is non-negative.
 
 // Time: O(a*n) where a is amount and n is amount of numbers.length
 // Space: O(a)
 
-const sumPossible = (amount, numbers, memo={}) => {
+const sumPossible = (amount, numbers, memo = {}) => {
   // If amount is in memo, return that value (either true or false)
   if (amount in memo) return memo[amount];
 
@@ -78,4 +84,3 @@ const sumPossible = (amount, numbers, memo={}) => {
   memo[amount] = false;
   return false;
 };
-
