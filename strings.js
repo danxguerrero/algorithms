11f9baf4b32559758e865078c466491a46d95490
Daniel Guerrero <danxguerrero@yahoo.com>
2024-11-01 23:08:02 -0400

    409. Longest Palindrome solution

--- strings.js
+++ strings.js
@@ -172,3 +172,58 @@ var isAnagram = function(s, t) {
 
     return true
 };
+
+// 409. Longest Palindrome
+// Given a string s which consists of lowercase or uppercase letters, return the length of the longest 
+// palindrome
+//  that can be built with those letters.
+
+// Letters are case sensitive, for example, "Aa" is not considered a palindrome.
+
+ 
+
+// Example 1:
+
+// Input: s = "abccccdd"
+// Output: 7
+// Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+// Example 2:
+
+// Input: s = "a"
+// Output: 1
+// Explanation: The longest palindrome that can be built is "a", whose length is 1.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 2000
+// s consists of lowercase and/or uppercase English letters only.
+
+var longestPalindrome = function(s) {
+    if (s.length === 1) return 1;
+
+    const map = {}
+    let count = 0
+
+    for (let char of s) {
+        map[char] = (map[char] || 0) + 1
+    }
+
+    let palindromeLength = 0;
+    let hasOdd = false;
+
+    for (let count of Object.values(map)) {
+        if (count % 2 === 0) {
+            palindromeLength += count;
+        } else {
+            palindromeLength += count - 1;
+            hasOdd = true;
+        }
+    }
+
+    if (hasOdd) {
+        palindromeLength++;
+    }
+
+    return palindromeLength;
+};
