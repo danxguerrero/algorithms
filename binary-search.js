07fdb58de3d7b49d3edfa4948f1c969cf6ca6499
Daniel Guerrero <danxguerrero@yahoo.com>
2024-11-04 16:54:42 -0500

    704. Binary Search solution

--- binary-search.js
+++ binary-search.js
@@ -23,3 +23,24 @@
 // -104 < nums[i], target < 104
 // All the integers in nums are unique.
 // nums is sorted in ascending order.
+
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+};
