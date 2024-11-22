5ffc96002f399d3a615d064f825635b2cde9f74a
Daniel Guerrero <danxguerrero@yahoo.com>
2024-11-22 15:04:53 -0500

    Encode and Decode Strings solution

--- arrays.js
+++ arrays.js
@@ -718,3 +718,72 @@ var combinationSum = function(candidates, target) {
 
   return result;
 };
+
+// Encode and Decode Strings
+
+// Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+
+// Please implement encode and decode
+
+// Example 1:
+
+// Input: ["neet","code","love","you"]
+
+// Output:["neet","code","love","you"]
+// Example 2:
+
+// Input: ["we","say",":","yes"]
+
+// Output: ["we","say",":","yes"]
+// Constraints:
+
+// 0 <= strs.length < 100
+// 0 <= strs[i].length < 200
+// strs[i] contains only UTF-8 characters.
+
+class Solution {
+  /**
+   * @param {string[]} strs
+   * @returns {string}
+   */
+
+  encode(strs) {
+      if (strs == []) return strs
+      let str = ""
+      let key = ""
+
+      for (let i = 0; i < strs.length; i++) {
+          const string = strs[i]
+          key = key + string.length + "#"
+          str = str + string
+      }
+
+      return key + "|" + str
+  } 
+
+  /**
+   * @param {string} str
+   * @returns {string[]}
+   */
+  decode(str) {
+      let [key, encodedStr] = str.split("|")
+      const result = []
+      // console.log(key, encodedStr)
+      let numStr = ""
+      
+      for (let i = 0; i < key.length; i++) {
+          
+          if (key[i] !== "#") {
+              numStr = numStr + key[i]
+          } else {
+              const num = parseInt(numStr)
+              numStr = ""
+
+              result.push(encodedStr.substring(0, num))
+              encodedStr = encodedStr.substring(num, encodedStr.length + 1)
+          }
+      }
+
+      return result
+  }
+}
