aa9b438f9d64e0ca0ca7acb1b38f1109314efff1
Daniel Guerrero <danxguerrero@yahoo.com>
2024-12-06 13:05:48 -0500

    102. Binary Tree Level Order Traversal solution

--- binary-tree.js
+++ binary-tree.js
@@ -298,3 +298,55 @@ var isBalanced = function(root) {
 
   return true;
 };
+
+// 102. Binary Tree Level Order Traversal
+
+// Given the root of a binary tree, return the level order traversal of its 
+// nodes' values. (i.e., from left to right, level by level).
+
+ 
+
+// Example 1:
+
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[9,20],[15,7]]
+// Example 2:
+
+// Input: root = [1]
+// Output: [[1]]
+// Example 3:
+
+// Input: root = []
+// Output: []
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 2000].
+// -1000 <= Node.val <= 1000
+
+var levelOrder = function(root) {
+  if (root == null) return [];
+
+  const result = [];
+
+  const queue = [root];
+
+  while (queue.length > 0) {
+      const level = [];
+      let size = queue.length;
+      
+      for (let i = 0; i < size; i++) {
+      const current = queue.shift();
+
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+      level.push(current.val);
+      }
+
+      result.push(level)
+  }
+
+  return result;
+};
